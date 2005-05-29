8eeaf8e7198ea78d7cf5220337592e7153fd445f
Aaron Boodman <boogs@youngpup.net>
2005-05-29 05:45:38 +0000

    Minor visual tweaks for dialogs

--- src/build.sh
+++ src/build.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 
 rm -rf build
-rm -rf ./content/*.patch
 mkdir -p build/chrome/greasemonkey
 cp install.rdf build/
 cp install.js build/
 cp -r content build/chrome/greasemonkey/
 cd build
-find * -not -name 'build.xml' | grep -v '\/CVS' | zip greasemonkey.xpi -@
+find * | grep -v '\/CVS' | zip greasemonkey.xpi -@
 mv greasemonkey.xpi ../
