714b6cf00216c78f0f5b969cb5aeaafaab2dd446
Aaron Boodman <boogs@youngpup.net>
2005-07-25 14:51:16 +0000

    *** empty log message ***

--- src/build.sh
+++ src/build.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
 rm -rf build
+cd content/scripts/
+find . | grep -v 'default-config\.xml' | grep -v CVS | grep -v '^.$' | xargs rm
+cd ../../
 mkdir -p build/chrome/greasemonkey
 cp install.rdf build/
 cp install.js build/
