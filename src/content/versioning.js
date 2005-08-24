01fbf4f60b3fa47f6b5f62cfd01529e577bc15cf
Aaron Boodman <boogs@youngpup.net>
2005-08-24 08:06:53 +0000

    *** empty log message ***

--- src/content/versioning.js
+++ src/content/versioning.js
@@ -50,7 +50,7 @@ function GM_updateVersion() {
   }
 
   // update the currently initialized version so we don't do this work again.
-  GM_prefRoot.setValue("version", "0.5");
+  GM_prefRoot.setValue("version", "0.5.1");
 
   GM_log("< GM_updateVersion");
 }
