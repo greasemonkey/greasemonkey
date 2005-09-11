b2a030bb4fdc79450b66b3e2e3779244b99c1219
Aaron Boodman <boogs@youngpup.net>
2005-09-11 01:58:30 +0000

    *** empty log message ***

--- src/install.js
+++ src/install.js
@@ -39,7 +39,7 @@ const APP_PACKAGE = "/greasemonkey.mozdev.org/greasemonkey";
 const APP_NAME = "/greasemonkey";
 // TODO: figure out how to extract this from install.rdf
 //   or generate install.rdf ...
-const APP_VERSION = "0.5.3";
+const APP_VERSION = "0.5.4";
 
 var instFlags = DELAYED_CHROME;
 
