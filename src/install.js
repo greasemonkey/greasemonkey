5eaee54d5b963f9bb3facd1fa33eeb52b5083a0a
Aaron Boodman <boogs@youngpup.net>
2006-07-26 17:51:18 +0000

    Updating version number

--- src/install.js
+++ src/install.js
@@ -7,7 +7,7 @@ const APP_PACKAGE = "/greasemonkey.mozdev.org/greasemonkey";
 const APP_NAME = "/greasemonkey";
 // TODO: figure out how to extract this from install.rdf
 //   or generate install.rdf ...
-const APP_VERSION = "0.6.5";
+const APP_VERSION = "0.6.5.20060726";
 
 var instFlags = DELAYED_CHROME;
 
