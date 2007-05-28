92b35bf45ead74b5efc68a943fcf55cad550e7a6
Aaron Boodman <boogs@youngpup.net>
2007-05-28 03:02:30 +0000

    Fix build script to work better with babelzilla localizations

--- src/build.sh
+++ src/build.sh
@@ -9,5 +9,13 @@ cp license.txt build/
 cp -r components build/
 cp -r chrome build/
 cd build
+
+# Generate locales for chrome.manifest from babelzilla directories, which
+# we assume have been placed in locale/.
+for entry in $(ls chrome/chromeFiles/locale/)
+do
+echo "locale  greasemonkey  "$entry"  chrome/chromeFiles/locale/"$entry"/" >> chrome.manifest
+done
+
 find * | grep -v 'CVS' | grep -v '\.svn' | grep -v ~$ | grep -v '#' | grep -v .DS_Store | zip greasemonkey.xpi -@
 mv greasemonkey.xpi ../
