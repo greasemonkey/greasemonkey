b04a2150b6fc8fe3ebaff19babe9ec67e867ffab
Aaron Boodman <boogs@youngpup.net>
2007-12-23 09:10:26 +0000

    Merging changeset 514 to trunk (build.sh improvements and getting rid of unused MochiKit directory)

--- src/build.sh
+++ src/build.sh
@@ -1,21 +1,56 @@
 #!/bin/sh
+GMMAX=0
+GMMIN=8
+GMREL=0
 
+GMNAME=greasemonkey
+
+# Copy base structure to a temporary build directory and change to it
 rm -rf build
 mkdir build
 cp chrome.manifest build/
 cp install.js build/
 cp install.rdf build/
 cp license.txt build/
+cp -r defaults build/
 cp -r components build/
 cp -r chrome build/
 cd build
 
 # Generate locales for chrome.manifest from babelzilla directories, which
 # we assume have been placed in locale/.
-for entry in $(ls chrome/chromeFiles/locale/)
-do
-echo "locale  greasemonkey  "$entry"  chrome/chromeFiles/locale/"$entry"/" >> chrome.manifest
+GMLOC=\"en-US\"
+for entry in $(ls chrome/chromeFiles/locale/); do
+  if [ $entry != en-US ]; then
+    echo "locale  $GMNAME  "$entry"  chrome/chromeFiles/locale/"$entry"/" >> chrome.manifest
+    GMLOC=$GMLOC,\ \"$entry\"
+  fi
 done
 
-find * | grep -v 'CVS' | grep -v '\.svn' | grep -v ~$ | grep -v '#' | grep -v .DS_Store | zip greasemonkey.xpi -@
-mv greasemonkey.xpi ../
+# Versioning checks
+GMBUILD=`date +"%04Y%02m%02d"`
+GMREGEXVER=[0-9]+\.[0-9]+\.[0-9]{8}\.[0-9]+
+
+checkGMVER () {
+  GMVER=`grep -Eo "$1" $2 | grep -Eo "$GMREGEXVER"`
+  if [ ! $GMVER ]; then
+    echo ERROR: $2 HAS INVALID VERSION!
+    exit 1
+  fi
+}
+
+sed -r -i "s/<em:version>.*<\/em:version>/<em:version>$GMMAX\.$GMMIN\.$GMBUILD\.$GMREL<\/em:version>/" install.rdf
+checkGMVER "<em:version>$GMREGEXVER<\/em:version>" install.rdf
+
+sed -r -i "s/const APP_VERSION =.*;/const APP_VERSION = \"$GMVER\";/" install.js
+checkGMVER "const APP_VERSION = \"$GMREGEXVER\";" install.js
+
+sed -r -i "s/const APP_LOCALES =.*;/const APP_LOCALES = [ $GMLOC ];/" install.js
+
+find . -name '.svn' -prune -or -name '.DS_Store' -or -name '*~' -or -name '#*' \
+  -or -print | zip $GMNAME-$GMVER.xpi -9X -@
+
+mv $GMNAME-$GMVER.xpi ../../downloads/
+
+echo Created $GMNAME-$GMVER.xpi
+exit 0
