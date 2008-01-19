b6eb3f0d06c69c871bf7b5c20008a7a95ecc67d1
Aaron Boodman <boogs@youngpup.net>
2008-01-19 21:57:08 +0000

    Fix another form of issue 63 that happened on windows this time.

--- src/chrome/chromeFiles/content/manage.js
+++ src/chrome/chromeFiles/content/manage.js
@@ -21,6 +21,7 @@ function handleOkButton() {
   var chkUninstallPrefs = document.getElementById('chkUninstallPrefs');
   for (var i = 0, script = null; (script = uninstallList[i]); i++) {
     file = getScriptBasedir(script);
+    file.normalize();
     if (!file.equals(getScriptDir())) {
       if (file.exists()) {
         file.remove(true); // file==base directory recursive delete
