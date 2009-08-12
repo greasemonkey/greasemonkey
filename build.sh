8cc502c92629b090e4d016af9ff7e56c13557862
Anthony Lieuallen <arantius@gmail.com>
2009-08-12 12:42:37 -0400

    Fix build.sh reference to the old source layout.

--- build.sh
+++ build.sh
@@ -23,7 +23,7 @@ GMLOC="en-US"
 for entry in locale/*; do
   entry=`basename $entry`
   if [ $entry != en-US ]; then
-    echo "locale  $GMNAME  $entry  chrome/chromeFiles/locale/$entry/" >> chrome.manifest
+    echo "locale  $GMNAME  $entry  locale/$entry/" >> chrome.manifest
     GMLOC="$GMLOC, $entry"
   fi
 done
