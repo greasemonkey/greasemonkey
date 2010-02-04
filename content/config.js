afdc88c220286bf0bfa5ea637dbad4c11172212d
Anthony Lieuallen <alieuall@google.com>
2010-02-05 05:54:53 +0800

    Open a "welcome" screen for new users, upon the first install.

--- content/config.js
+++ content/config.js
@@ -389,13 +389,26 @@ Config.prototype = {
     // this is the last version which has been run at least once
     var initialized = GM_prefRoot.getValue("version", "0.0");
 
+    if ("0.0" == initialized) {
+      // this is the first launch.  show the welcome screen.
+
+      // find an open window.
+      var windowManager = Components
+           .classes['@mozilla.org/appshell/window-mediator;1']
+           .getService(Components.interfaces.nsIWindowMediator);
+      var chromeWin = windowManager.getMostRecentWindow("navigator:browser");
+      // if we found it, use it to open a welcome tab
+      if (chromeWin.gBrowser) {
+        chromeWin.gBrowser.addTab("http://wiki.greasespot.net/Welcome");
+      }
+    }
+
     if (GM_compareVersions(initialized, "0.8") == -1)
       this._pointEightBackup();
 
     // update the currently initialized version so we don't do this work again.
     var extMan = Components.classes["@mozilla.org/extensions/manager;1"]
       .getService(Components.interfaces.nsIExtensionManager);
-
     var item = extMan.getItemForID(GM_GUID);
     GM_prefRoot.setValue("version", item.version);
 
