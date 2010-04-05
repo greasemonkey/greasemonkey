1f0efae2d86a580a06070cee5a9ca37b149b303b
Erik Vold <erikvvold@gmail.com>
2010-04-05 16:57:06 -0700

    Fixes #1038, adding 'greasemonkey.unmhtIsGreaseable' to about:config (default false) which if true allows user scripts to be executed on pages using the 'unmht' uri scheme.

--- defaults/preferences/greasemonkey.js
+++ defaults/preferences/greasemonkey.js
@@ -1,3 +1,4 @@
 pref("extensions.{e4a8a97b-f2ed-450b-b12d-ee082ba24781}.description", "chrome://greasemonkey/locale/greasemonkey.properties");
 pref("greasemonkey.aboutIsGreaseable", false);
 pref("greasemonkey.fileIsGreaseable", false);
+pref("greasemonkey.unmhtIsGreaseable", false);
