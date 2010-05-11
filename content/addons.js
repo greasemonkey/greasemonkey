dc7927052768530d7d78c05f556dd7233b9ba95e
Anthony Lieuallen <arantius@gmail.com>
2010-05-11 14:29:38 -0400

    Remove obsolete locale strings.  Add, and use, new ones for addons.

--- content/addons.js
+++ content/addons.js
@@ -1,6 +1,12 @@
 // Globals.
 var GM_config = GM_getConfig();
 
+var GM_stringBundle = Components
+    .classes["@mozilla.org/intl/stringbundle;1"]
+    .getService(Components.interfaces.nsIStringBundleService)
+    .createBundle("chrome://greasemonkey/locale/gm-manage.properties");
+function GM_string(key) { return GM_stringBundle.GetStringFromName(key); }
+
 (function() {
 // Override some built-in functions, with a closure reference to the original
 // function, to either handle or delegate the call.
@@ -187,9 +193,9 @@ var greasemonkeyAddons = {
     button = item.ownerDocument.getAnonymousElementByAttribute(
         item, 'command', 'cmd_options');
     if (!button) return;
-    button.setAttribute('label', 'Edit');
-    button.setAttribute('accesskey', 'E');
-    button.setAttribute('tooltiptext', 'Edit the selected User Script');
+    button.setAttribute('label', GM_string('Edit'));
+    button.setAttribute('accesskey', GM_string('Edit.accesskey'));
+    button.setAttribute('tooltiptext', GM_string('Edit.tooltip'));
     button.setAttribute('command', 'cmd_userscript_edit');
     button.setAttribute('disabled', 'false');
 
@@ -197,21 +203,21 @@ var greasemonkeyAddons = {
     button = item.ownerDocument.getAnonymousElementByAttribute(
         item, 'command', 'cmd_enable');
     if (!button) return;
-    button.setAttribute('tooltiptext', 'Enable the selected User Script');
+    button.setAttribute('tooltiptext', GM_string('Enable.tooltip'));
     button.setAttribute('command', 'cmd_userscript_enable');
     button.setAttribute('disabled', 'false');
 
     button = item.ownerDocument.getAnonymousElementByAttribute(
         item, 'command', 'cmd_disable');
     if (!button) return;
-    button.setAttribute('tooltiptext', 'Disable the selected User Script');
+    button.setAttribute('tooltiptext', GM_string('Disable.tooltip'));
     button.setAttribute('command', 'cmd_userscript_disable');
     button.setAttribute('disabled', 'false');
 
     button = item.ownerDocument.getAnonymousElementByAttribute(
         item, 'command', 'cmd_uninstall');
     if (!button) return;
-    button.setAttribute('tooltiptext', 'Uninstall the selected User Script');
+    button.setAttribute('tooltiptext', GM_string('Uninstall.tooltip'));
     button.setAttribute('command', 'cmd_userscript_uninstall');
     button.setAttribute('disabled', 'false');
   },
@@ -219,7 +225,7 @@ var greasemonkeyAddons = {
   doCommand: function(command) {
     var script = greasemonkeyAddons.findSelectedScript();
     if (!script) {
-      alert('Crap, something went wrong!');
+      dump("greasemonkeyAddons.doCommand() could not find selected script.\n");
       return;
     }
 
@@ -261,7 +267,7 @@ var greasemonkeyAddons = {
   buildContextMenu: function(aEvent) {
     var script = greasemonkeyAddons.findSelectedScript();
     if (!script) {
-      alert('Crap, something went wrong!');
+      dump("greasemonkeyAddons.buildContextMenu() could not find selected script.\n");
       return;
     }
 
@@ -272,7 +278,8 @@ var greasemonkeyAddons = {
 
     function addMenuItem(label, command) {
       var menuitem = document.createElement('menuitem');
-      menuitem.setAttribute('label', label);
+      menuitem.setAttribute('label', GM_string(label));
+      menuitem.setAttribute('accesskey', GM_string(label+'.accesskey'));
       menuitem.setAttribute('command', command);
       popup.appendChild(menuitem);
     }
