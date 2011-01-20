aef88fda2c5b0927ee0a39b4b2cd237b4a90cdc5
Erik Vold <erikvvold@gmail.com>
2011-01-19 20:20:15 -0800

    Closes #1249 fixing errors in the Error Console when using GM_notification on OSX without Growl

--- modules/GM_notification.js
+++ modules/GM_notification.js
@@ -1,24 +1,23 @@
-// JSM exported symbols
 var EXPORTED_SYMBOLS = ["GM_notification"];
 
-const alertsServ = Components.classes["@mozilla.org/alerts-service;1"]
-    .getService(Components.interfaces.nsIAlertsService);
+const {classes: Cc, interfaces: Ci} = Components;
 
-function alert(msg) {
-  Components.classes["@mozilla.org/embedcomp/prompt-service;1"]
-      .getService(Components.interfaces.nsIPromptService)
-      .alert(null, "Greasemonkey alert", msg);
+try {
+  var notify = Cc["@mozilla.org/alerts-service;1"]
+      .getService(Ci.nsIAlertsService)
+      .showAlertNotification;
+} catch (e) {
+  var notify = function() {
+    Cc["@mozilla.org/embedcomp/prompt-service;1"]
+        .getService(Ci.nsIPromptService)
+        .alert(null, "Greasemonkey alert", arguments[2]);
+  }
 }
 
 function GM_notification(aMsg, aTitle) {
   var title = aTitle ? "" + aTitle : "Greasemonkey";
   var message = aMsg ? "" + aMsg : "";
-  try {
-    alertsServ.showAlertNotification(
-        "chrome://greasemonkey/skin/icon_medium.png",
-        title, message, false, "", null);
-  } catch (e) {
-    // In case e.g. Growl is not installed on a Mac.
-    alert(title + "\n" + message);
-  }
+  notify(
+      "chrome://greasemonkey/skin/icon_medium.png",
+      title, message, false, "", null);
 };
