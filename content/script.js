greasemonkey/greasemonkey

commit 54b63f7f579e8caa08cbf0a468073342d8c2df77

Use GM_getConfig() rather than passing config references around. Define, use GM_scriptDir() helper.

content/script.js
@@ -1,5 +1,4 @@
-function Script(config) {
-  this._config = config;
+function Script() {
   this._observers = [];
 
   this._downloadURL = null; // Only for scripts not installed
@@ -35,7 +34,9 @@ Script.prototype = {
     return this._includes.some(test) && !this._excludes.some(test);
   },
 
-  _changed: function(event, data) { this._config._changed(this, event, data); },
+  _changed: function(event, data) {
+    GM_getConfig()._changed(this, event, data);
+  },
 
   get name() { return this._name; },
   get namespace() { return this._namespace; },
@@ -72,7 +73,7 @@ Script.prototype = {
   get editFile() { return this._file; },
 
   get _basedirFile() {
-    var file = this._config._scriptDir;
+    var file = GM_scriptDir();
     file.append(this._basedir);
     file.normalize();
     return file;
@@ -106,12 +107,12 @@ Script.prototype = {
   },
 
   _initFile: function(tempFile) {
-    var file = this._config._scriptDir;
     var name = this._initFileName(this._name, false);
+    this._basedir = name;
 
+    var file = GM_scriptDir();
     file.append(name);
     file.createUnique(Components.interfaces.nsIFile.DIRECTORY_TYPE, 0755);
-    this._basedir = file.leafName;
 
     file.append(name + ".user.js");
     file.createUnique(Components.interfaces.nsIFile.NORMAL_FILE_TYPE, 0644);

content/config.js
@@ -1,7 +1,7 @@
 function Config() {
   this._saveTimer = null;
   this._scripts = null;
-  this._configFile = this._scriptDir;
+  this._configFile = GM_scriptDir();
   this._configFile.append("config.xml");
   this._initScriptDir();
 
@@ -69,7 +69,7 @@ Config.prototype = {
     this._scripts = [];
 
     for (var node = null; node = nodes.iterateNext(); ) {
-      var script = new Script(this);
+      var script = new Script();
 
       script._filename = node.getAttribute("filename");
       script._basedir = node.getAttribute("basedir") || ".";
@@ -231,7 +231,7 @@ Config.prototype = {
   },
 
   parse: function(source, uri, updating) {
-    var script = new Script(this);
+    var script = new Script();
 
     if (uri) {
       script._downloadURL = uri.spec;
@@ -388,7 +388,7 @@ Config.prototype = {
     this._changed(script, "uninstall", null);
 
     // watch out for cases like basedir="." and basedir="../gm_scripts"
-    if (!script._basedirFile.equals(this._scriptDir)) {
+    if (!script._basedirFile.equals(GM_scriptDir())) {
       // if script has its own dir, remove the dir + contents
       script._basedirFile.remove(true);
     } else {
@@ -432,20 +432,11 @@ Config.prototype = {
     this._changed(script, "move", to);
   },
 
-  get _scriptDir() {
-    var file = Components.classes["@mozilla.org/file/directory_service;1"]
-                         .getService(Components.interfaces.nsIProperties)
-                         .get("ProfD", Components.interfaces.nsILocalFile);
-    file.append("gm_scripts");
-    return file;
-  },
-
   /**
    * Create an empty configuration if none exist.
    */
   _initScriptDir: function() {
-    var dir = this._scriptDir;
-
+    var dir = GM_scriptDir();
     if (!dir.exists()) {
       dir.create(Components.interfaces.nsIFile.DIRECTORY_TYPE, 0755);
 
@@ -539,7 +530,7 @@ Config.prototype = {
    * are paranoid and backup the folder the first time 0.8 runs.
    */
   _pointEightBackup: function() {
-    var scriptDir = this._scriptDir;
+    var scriptDir = GM_scriptDir();
     var scriptDirBackup = scriptDir.clone();
     scriptDirBackup.leafName += "_08bak";
     if (scriptDir.exists() && !scriptDirBackup.exists())

content/scriptdownloader.js
@@ -198,14 +198,14 @@ GM_ScriptDownloader.prototype.checkDependencyURL = function(url) {
   }
 };
 
-GM_ScriptDownloader.prototype.finishInstall = function(){
+GM_ScriptDownloader.prototype.finishInstall = function() {
   if (this.updateScript) {
     // Inject the script now that we have the new dependencies
-    this.script._config.injectScript(this.script);
+    GM_getConfig().injectScript(this.script);
     this.delayInjection = false;
 
     // Save new values to config.xml
-    this.script._config._save();
+    GM_getConfig()._save();
   } else if (this.installOnCompletion_) {
     this.installScript();
   }

content/utils.js
@@ -361,6 +361,18 @@ function GM_sha1(unicode) {
 }
 GM_sha1 = GM_memoize(GM_sha1);
 
+GM_scriptDirCache = null;
+function GM_scriptDir() {
+  if (!GM_scriptDirCache) {
+    GM_scriptDirCache = Components
+        .classes["@mozilla.org/file/directory_service;1"]
+        .getService(Components.interfaces.nsIProperties)
+        .get("ProfD", Components.interfaces.nsILocalFile);
+    GM_scriptDirCache.append("gm_scripts");
+  }
+  return GM_scriptDirCache.clone();
+}
+
 // Decorate a function with a memoization wrapper, with a limited-size cache
 // to reduce peak memory utilization.  Simple usage:
 //

commit 92e3a5aa2e4d1612ab7ddfcd32551c1afc29ab9e

Closes #1250 fixing bug introduced in 54b63f7f579e8caa08cbf0a468073342d8c2df77

content/script.js
@@ -260,6 +260,7 @@ Script.prototype = {
     var file = GM_scriptDir();
     file.append(name);
     file.createUnique(Components.interfaces.nsIFile.DIRECTORY_TYPE, 0755);
+    this._basedir = file.leafName;
 
     file.append(name + ".user.js");
     file.createUnique(Components.interfaces.nsIFile.NORMAL_FILE_TYPE, 0644);