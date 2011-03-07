5a3fc10153ad97a8e780883d472fdebf4b4cd22a
Mehdi Mulani <mmmulani@uwaterloo.ca>
2011-03-07 11:20:27 -0800

    Changed notify to see if Growl is running. #1264

--- modules/GM_notification.js
+++ modules/GM_notification.js
@@ -2,15 +2,22 @@ var EXPORTED_SYMBOLS = ["GM_notification"];
 
 const {classes: Cc, interfaces: Ci} = Components;
 
-try {
-  var notify = Cc["@mozilla.org/alerts-service;1"]
-      .getService(Ci.nsIAlertsService)
-      .showAlertNotification;
-} catch (e) {
-  var notify = function() {
-    Cc["@mozilla.org/embedcomp/prompt-service;1"]
-        .getService(Ci.nsIPromptService)
-        .alert(null, "Greasemonkey alert", arguments[2]);
+// The first time this runs, we check if nsIAlertsService is installed and
+// works. If it fails, we re-define notify to use nsIPromptService always.
+// We check to see if nsIAlertsService works because of the case where Growl
+// is installed. See also https://bugzilla.mozilla.org/show_bug.cgi?id=597165
+function notify() {
+  try {
+    Cc["@mozilla.org/alerts-service;1"]
+        .getService(Ci.nsIAlertsService)
+        .showAlertNotification.apply(null, arguments);
+  } catch (e) {
+    notify = function() {
+      Cc["@mozilla.org/embedcomp/prompt-service;1"]
+          .getService(Ci.nsIPromptService)
+          .alert(null, "Greasemonkey alert", arguments[2]);
+    };
+    notify.apply(null, arguments);
   }
 }
 
