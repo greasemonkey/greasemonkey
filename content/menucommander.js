9b848aaa9875888564de90561fcc751443ef16a8
Anthony Lieuallen <arantius@gmail.com>
2011-04-05 10:17:29 -0400

    Do not show 'no commands' message; just disable opening the menu.
    Fixes #1327

--- content/menucommander.js
+++ content/menucommander.js
@@ -16,19 +16,13 @@ GM_MenuCommander.onPopupShowing = function(aMenuPopup) {
   GM_emptyEl(aMenuPopup);
 
   // Add menu items for commands for the active window.
-  var flag = false;
+  var haveCommands = false;
   GM_BrowserUI.gmSvc.withAllMenuCommandsForWindowId(
       GM_windowId(gBrowser.contentWindow),
       function(index, command) {
         if (command.frozen) return;
         aMenuPopup.appendChild(GM_MenuCommander.createMenuItem(command));
-        flag = true;
+        haveCommands = true;
       });
-  if (!flag) {
-    var warning = document.createElement("menuitem");
-    warning.setAttribute(
-        "label", GM_BrowserUI.bundle.getString("menuitem.nocommands"));
-    warning.setAttribute("disabled", "true");
-    aMenuPopup.appendChild(warning);
-  }
+  aMenuPopup.parentNode.disabled = !haveCommands;
 };
