732a05e3fd7f8cd61e8fcc8097c20871b4111717
Anthony Lieuallen <alieuall@google.com>
2011-08-22 14:56:16 -0400

    Import util.

--- modules/addons4.js
+++ modules/addons4.js
@@ -17,6 +17,7 @@ var EXPORTED_SYMBOLS = [
 Components.utils.import('resource://gre/modules/AddonManager.jsm');
 Components.utils.import('resource://gre/modules/Services.jsm');
 Components.utils.import('resource://gre/modules/XPCOMUtils.jsm');
+Components.utils.import('resource://greasemonkey/util.js');
 
 var Cc = Components.classes;
 var Ci = Components.interfaces;
