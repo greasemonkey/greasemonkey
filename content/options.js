60770b3f60ec3ffc03afcac698984fe65600d0db
Anthony Lieuallen <alieuall@google.com>
2011-09-16 12:07:52 -0400

    Clean up (min interval) options.
    * Simpler get/set script.
    * Consistent apply-on-OK (not partial instant apply).
    * Consistent single quotes.
    * Display value beside slider rather than in popup.

--- content/options.js
+++ content/options.js
@@ -1,42 +1,37 @@
 Components.utils.import('resource://greasemonkey/prefmanager.js');
 Components.utils.import('resource://greasemonkey/util.js');
 
-var GM_setOptionsYet = false;
 function GM_loadOptions() {
-  document.getElementById("check-uninstall")
-      .checked = GM_prefRoot.getValue("uninstallPreferences");
+  document.getElementById('check-uninstall')
+      .checked = GM_prefRoot.getValue('uninstallPreferences');
 
-  document.getElementById("globalExcludes")
+  document.getElementById('globalExcludes')
       .pages = GM_util.getService().config.globalExcludes;
 
-  document.getElementById("check-update")
-      .checked = GM_prefRoot.getValue("enableUpdateChecking");
+  document.getElementById('check-update')
+      .checked = GM_prefRoot.getValue('enableUpdateChecking');
 
-  document.getElementById("slide-updateInterval")
-      .value = GM_prefRoot.getValue("minDaysBetweenUpdateChecks");
+  document.getElementById('slide-updateInterval')
+      .value = GM_prefRoot.getValue('minDaysBetweenUpdateChecks');
 
-  document.getElementById("txt-updateInterval")
-      .setAttribute("label", GM_prefRoot.getValue("minDaysBetweenUpdateChecks"));
-  GM_setOptionsYet = true;
+  GM_setMinUpdateIntervalLabel();
 }
 
 function GM_saveOptions(checkbox) {
-  GM_prefRoot.setValue("uninstallPreferences",
-      !!document.getElementById("check-uninstall").checked);
+  GM_prefRoot.setValue('uninstallPreferences',
+      !!document.getElementById('check-uninstall').checked);
   GM_util.getService().config.globalExcludes =
-      document.getElementById("globalExcludes").pages;
+      document.getElementById('globalExcludes').pages;
+  GM_prefRoot.setValue('enableUpdateChecking',
+      !!document.getElementById('check-update').checked);
+  GM_prefRoot.setValue("minDaysBetweenUpdateChecks", GM_getMinUpdateDays());
 }
 
-function GM_setUpdatePrefs(checkbox) {
-  GM_prefRoot.setValue("enableUpdateChecking",
-      !!document.getElementById("check-update").checked);
+function GM_getMinUpdateDays() {
+  return parseInt(document.getElementById('slide-updateInterval').value);
 }
 
-function GM_setMinUpdateInterval(input) {
-  if (!GM_setOptionsYet) return;
-  var days = parseInt(document.getElementById("slide-updateInterval").value);
-
-  document.getElementById("txt-updateInterval")
-    .setAttribute("label", days);
-  GM_prefRoot.setValue("minDaysBetweenUpdateChecks", days);
+function GM_setMinUpdateIntervalLabel() {
+  document.getElementById('txt-updateInterval')
+      .setAttribute('value', GM_getMinUpdateDays());
 }
