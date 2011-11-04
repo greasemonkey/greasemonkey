f86b144e4052f74ab67b6f1536db4d738283eda2
Anthony Lieuallen <alieuall@google.com>
2011-11-04 12:41:22 -0400

    Make a single io service, not one per run.

--- modules/util/uriFromUrl.js
+++ modules/util/uriFromUrl.js
@@ -2,11 +2,11 @@ Components.utils.import('resource://greasemonkey/util.js');
 
 var EXPORTED_SYMBOLS = ['uriFromUrl'];
 
-function uriFromUrl(url, base) {
-  var ioService = Components.classes["@mozilla.org/network/io-service;1"]
-      .getService(Components.interfaces.nsIIOService);
-  var baseUri = null;
+var ioService = Components.classes["@mozilla.org/network/io-service;1"]
+    .getService(Components.interfaces.nsIIOService);
 
+function uriFromUrl(url, base) {
+  var baseUri = null;
   if (typeof base === "string") {
     baseUri = GM_util.uriFromUrl(base);
   } else if (base) {
