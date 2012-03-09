4ef5e31466f5f913c7682cc09b9046ea60945f34
Anthony Lieuallen <alieuall@google.com>
2012-03-09 10:22:51 -0500

    Curry extra arguments for GM_util.timeout's callback.
    Refs #1209

--- modules/util/timeout.js
+++ modules/util/timeout.js
@@ -3,9 +3,12 @@ Components.utils.import('resource://greasemonkey/util.js');
 const EXPORTED_SYMBOLS = ['timeout'];
 
 function timeout(aCallback, aDelay) {
+  var extraArgs = Array.prototype.slice.call(arguments, 2);
+  var curriedCallback = GM_util.hitch(null, aCallback, extraArgs);
+
   Components.classes["@mozilla.org/timer;1"]
       .createInstance(Components.interfaces.nsITimer)
       .initWithCallback(
-          {'notify': aCallback}, aDelay,
+          {'notify': curriedCallback}, aDelay,
           Components.interfaces.nsITimer.TYPE_ONE_SHOT);
 }
