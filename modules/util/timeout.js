2db0ff688de4c9268061571723e997ff6c1aaa5c
Anthony Lieuallen <alieuall@google.com>
2012-05-11 10:31:04 -0400

    Revert "Curry extra arguments for GM_util.timeout's callback."
    This reverts commit 4ef5e31466f5f913c7682cc09b9046ea60945f34.

--- modules/util/timeout.js
+++ modules/util/timeout.js
@@ -3,12 +3,9 @@ Components.utils.import('resource://greasemonkey/util.js');
 const EXPORTED_SYMBOLS = ['timeout'];
 
 function timeout(aCallback, aDelay) {
-  var extraArgs = Array.prototype.slice.call(arguments, 2);
-  var curriedCallback = GM_util.hitch(null, aCallback, extraArgs);
-
   Components.classes["@mozilla.org/timer;1"]
       .createInstance(Components.interfaces.nsITimer)
       .initWithCallback(
-          {'notify': curriedCallback}, aDelay,
+          {'notify': aCallback}, aDelay,
           Components.interfaces.nsITimer.TYPE_ONE_SHOT);
 }
