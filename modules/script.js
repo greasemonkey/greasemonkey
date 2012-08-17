be7f924cd46ca52b067301e959245a356b5cd1f6
Anthony Lieuallen <arantius@gmail.com>
2012-08-17 11:34:08 -0400

    Enforce that the download, not update, URL is secure.
    Fixes #1555

--- modules/script.js
+++ modules/script.js
@@ -499,7 +499,7 @@ Script.prototype.isRemoteUpdateAllowed = function(aForced) {
 
   var ioService = Components.classes["@mozilla.org/network/io-service;1"]
       .getService(Components.interfaces.nsIIOService);
-  var scheme = ioService.extractScheme(this.updateURL);
+  var scheme = ioService.extractScheme(this._downloadURL);
   switch (scheme) {
   case 'about':
   case 'chrome':
