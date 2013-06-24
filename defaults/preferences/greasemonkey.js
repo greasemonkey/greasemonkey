3f3914b265cd96d5dad740e744804700aadccf7a
jerone <jeronevw@hotmail.com>
2013-06-24 21:17:11 +0200

    Custom meta-data block format. Fixes #1753

--- defaults/preferences/greasemonkey.js
+++ defaults/preferences/greasemonkey.js
@@ -3,6 +3,8 @@ pref("extensions.greasemonkey.coralCacheWorks", true);
 pref("extensions.greasemonkey.enableScriptRefreshing", true);
 pref("extensions.greasemonkey.fileIsGreaseable", false);
 pref("extensions.greasemonkey.globalExcludes", '[]');
+pref("extensions.greasemonkey.newScript.removeUnused", true);
+pref("extensions.greasemonkey.newScript.format", '// ==UserScript==\n// @name        %name%\n// @namespace   %namespace%\n// @description %description%\n// @include     %include%\n// @exclude     %exclude%\n// @version     1\n// ==/UserScript==');
 pref("extensions.greasemonkey.haveInsertedToolbarbutton", false);
 pref("extensions.greasemonkey.installDelay", 5);
 pref("extensions.greasemonkey.logChrome", false);
