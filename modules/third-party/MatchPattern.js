greasemonkey/greasemonkey

commit 79ea1be8b106331f1e25ea3d46324cd3ed974fbb

Correctly handle *. in the host part of match patterns.
Fixes #1899.

modules/third-party/MatchPattern.js
@@ -94,7 +94,15 @@ function MatchPattern(pattern) {
 
   this._scheme = scheme;
   if (host) {
-    this._hostExpr = GM_convert2RegExp(host.replace(/^\*\./, "*"));
+    // We have to manually create the hostname regexp (instead of using
+    // GM_convert2RegExp) to properly handle *.example.tld, which should match
+    // example.tld and any of its subdomains, but not anotherexample.tld.
+    this._hostExpr = new RegExp("^" +
+        // The only character that's valid in a hostname and needs to be escaped
+        // for regexps is ".".
+        host.replace(/\./g, "\\.")
+        // Then, handle "*.", which by now has been escaped to "*\.".
+            .replace(/\*\\./, "(.*\\.)?") + "$", "i");
   } else {
     // If omitted, then it means "", an alias for localhost.
     this._hostExpr = /^$/;

commit 05edf947ea0ec6a9db4c262f359ea5f35218efbd

Fix full wildcard host patterns in @match.
This was broken by 79ea1be8b106331f1e25ea3d46324cd3ed974fbb.
Fixes #1912.

modules/third-party/MatchPattern.js
@@ -98,11 +98,14 @@ function MatchPattern(pattern) {
     // GM_convert2RegExp) to properly handle *.example.tld, which should match
     // example.tld and any of its subdomains, but not anotherexample.tld.
     this._hostExpr = new RegExp("^" +
-        // The only character that's valid in a hostname and needs to be escaped
-        // for regexps is ".".
-        host.replace(/\./g, "\\.")
-        // Then, handle "*.", which by now has been escaped to "*\.".
-            .replace(/\*\\./, "(.*\\.)?") + "$", "i");
+        // Two characters in the host portion need special treatment:
+        //   - ". should not be treated as a wildcard, so we escape it to \.
+        //   - if the hostname only consists of "*" (i.e. full wildcard),
+        //     replace it with .*
+        host.replace(/\./g, "\\.").replace(/^\*$/, ".*")
+        // Then, handle the special case of "*." (any or no subdomain) for match
+        // patterns. "*." has been escaped to "*\." by the replace above.
+            .replace("*\\.", "(.*\\.)?") + "$", "i");
   } else {
     // If omitted, then it means "", an alias for localhost.
     this._hostExpr = /^$/;