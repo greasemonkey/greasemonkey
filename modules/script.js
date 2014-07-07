greasemonkey/greasemonkey

commit 1ddf46312573d1ddbb5dc3e52b19cd68cace743c

Fail gracefully when unable to parse the download URL.
Fixes #1930

modules/script.js
@@ -501,7 +501,13 @@ Script.prototype.isRemoteUpdateAllowed = function(aForced) {
 
   var ioService = Components.classes["@mozilla.org/network/io-service;1"]
       .getService(Components.interfaces.nsIIOService);
-  var scheme = ioService.extractScheme(this.downloadURL);
+  try {
+    var scheme = ioService.extractScheme(this.downloadURL);
+  } catch (e) {
+    // Invalid URL, probably an old legacy install.  Do not update.
+    return false;
+  }
+
   switch (scheme) {
   case 'about':
   case 'chrome':
@@ -522,8 +528,6 @@ Script.prototype.isRemoteUpdateAllowed = function(aForced) {
     // Anything not listed: default to not allow.
     return false;
   }
-
-  return true;
 };
 
 Script.prototype.updateFromNewScript = function(newScript, safeWin) {

commit fd28991bfbd710204ca67b8b068353d09a0176e0

isRemoteUpdateAllowed(): all cases return a value
Fix breakage introduced in 1ddf46312573d1ddbb5dc3e52b19cd68cace743c

Refs #1947
Fixes #1938

modules/script.js
@@ -517,12 +517,10 @@ Script.prototype.isRemoteUpdateAllowed = function(aForced) {
   case 'ftp':
   case 'http':
     // These schemes are OK only if the user opts in.
-    if (GM_prefRoot.getValue('requireSecureUpdates')) {
-      return false;
-    }
-    break;
+    return !GM_prefRoot.getValue('requireSecureUpdates');;
   case 'https':
     // HTTPs is always OK.
+    return true;
     break;
   default:
     // Anything not listed: default to not allow.