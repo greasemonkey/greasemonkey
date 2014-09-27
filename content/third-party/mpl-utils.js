greasemonkey/greasemonkey

commit d2822b79eaec63ab2f8aa7646a82bdf3350a312e

Move GM_openInTab to miscapis.js.

modules/miscapis.js
@@ -2,12 +2,13 @@ var Cu = Components.utils;
 
 Cu.import("resource://gre/modules/Services.jsm");
 
+Cu.import("resource://greasemonkey/third-party/getChromeWinForContentWin.js");
 Cu.import('resource://greasemonkey/prefmanager.js');
 Cu.import("resource://greasemonkey/util.js");
 
 
 var EXPORTED_SYMBOLS = [
-    'GM_addStyle', 'GM_console', 'GM_Resources',
+    'GM_addStyle', 'GM_console', 'GM_openInTab', 'GM_Resources',
     'GM_ScriptLogger', 'GM_ScriptStorage', 'GM_ScriptStoragePrefs'];
 
 
@@ -285,4 +286,35 @@ function GM_console(script) {
 }
 
 GM_console.prototype.log = function() {
+};
+
+// \\ // \\ // \\ // \\ // \\ // \\ // \\ // \\ // \\ // \\ // \\ // \\ // \\ //
+
+function GM_openInTab(safeContentWin, url, aLoadInBackground) {
+  if ('undefined' == typeof aLoadInBackground) aLoadInBackground = null;
+
+  // Resolve URL relative to the location of the content window.
+  var baseUri = Services.io.newURI(safeContentWin.location.href, null, null);
+  var uri = Services.io.newURI(url, null, baseUri);
+
+  // Get the chrome window currently corresponding to the content window, as
+  // this might have changed since the script was injected (e.g. by moving
+  // the tab to a different window).
+  var chromeWin = getChromeWinForContentWin(safeContentWin);
+  var browser = chromeWin.gBrowser;
+  var currentTab = browser.tabs[
+      browser.getBrowserIndexForDocument(safeContentWin.top.document)];
+  var newTab = browser.loadOneTab(
+      uri.spec, {'inBackground': aLoadInBackground});
+  var newWin = GM_windowForTab(newTab, browser);
+
+  var afterCurrent = Cc["@mozilla.org/preferences-service;1"]
+      .getService(Ci.nsIPrefService)
+      .getBranch("browser.tabs.")
+      .getBoolPref("insertRelatedAfterCurrent");
+  if (afterCurrent) {
+    browser.moveTabTo(newTab, currentTab._tPos + 1);
+  }
+
+  return newWin;
 };

components/greasemonkey.js
@@ -143,7 +143,7 @@ function createSandbox(aScript, aContentWin, aChromeWin, aUrl) {
   }
   if (GM_util.inArray(aScript.grants, 'GM_openInTab')) {
     sandbox.GM_openInTab = GM_util.hitch(
-        null, openInTab, aContentWin);
+        null, GM_openInTab, aContentWin);
   }
   if (GM_util.inArray(aScript.grants, 'GM_xmlhttpRequest')) {
     sandbox.GM_xmlhttpRequest = GM_util.hitch(
@@ -162,35 +162,6 @@ function isTempScript(uri) {
   var file = gFileProtocolHandler.getFileFromURLSpec(uri.spec);
   return gTmpDir.contains(file, true);
 }
-
-function openInTab(safeContentWin, url, aLoadInBackground) {
-  if ('undefined' == typeof aLoadInBackground) aLoadInBackground = null;
-
-  // Resolve URL relative to the location of the content window.
-  var baseUri = gIoService.newURI(safeContentWin.location.href, null, null);
-  var uri = gIoService.newURI(url, null, baseUri);
-
-  // Get the chrome window currently corresponding to the content window, as
-  // this might have changed since the script was injected (e.g. by moving
-  // the tab to a different window).
-  var chromeWin = getChromeWinForContentWin(safeContentWin);
-  var browser = chromeWin.gBrowser;
-  var currentTab = browser.tabs[
-      browser.getBrowserIndexForDocument(safeContentWin.top.document)];
-  var newTab = browser.loadOneTab(
-      uri.spec, {'inBackground': aLoadInBackground});
-  var newWin = GM_windowForTab(newTab, browser);
-
-  var afterCurrent = Cc["@mozilla.org/preferences-service;1"]
-      .getService(Ci.nsIPrefService)
-      .getBranch("browser.tabs.")
-      .getBoolPref("insertRelatedAfterCurrent");
-  if (afterCurrent) {
-    browser.moveTabTo(newTab, currentTab._tPos + 1);
-  }
-
-  return newWin;
-};
 
 function registerMenuCommand(
     wrappedContentWin, script,

commit 3f512a110fd5893c62073eee16101bbf0bed190b

Fix reference errors in GM_openInTab and remove GM_windowForTab.
This was broken by d2822b79eaec63ab2f8aa7646a82bdf3350a312e.

content/third-party/mpl-utils.js
@@ -55,14 +55,3 @@ function GM_openFolder(aFile) {
     }
   }
 }
-
-function GM_windowForTab(aTab, aBrowser) {
-  var browser = aBrowser || gBrowser;
-  // Source:
-  // http://mxr.mozilla.org/mozilla-central/source/browser/base/content/browser.js#4448
-  return browser
-    .getBrowserForTab(aTab)
-    .docShell
-    .QueryInterface(Ci.nsIInterfaceRequestor)
-    .getInterface(Ci.nsIDOMWindow);
-}

modules/miscapis.js
@@ -306,10 +306,10 @@ function GM_openInTab(safeContentWin, url, aLoadInBackground) {
       browser.getBrowserIndexForDocument(safeContentWin.top.document)];
   var newTab = browser.loadOneTab(
       uri.spec, {'inBackground': aLoadInBackground});
-  var newWin = GM_windowForTab(newTab, browser);
+  var newWin = browser.getBrowserForTab(newTab).contentWindow;
 
-  var afterCurrent = Cc["@mozilla.org/preferences-service;1"]
-      .getService(Ci.nsIPrefService)
+  var afterCurrent = Components.classes["@mozilla.org/preferences-service;1"]
+      .getService(Components.interfaces.nsIPrefService)
       .getBranch("browser.tabs.")
       .getBoolPref("insertRelatedAfterCurrent");
   if (afterCurrent) {