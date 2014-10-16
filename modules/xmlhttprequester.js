greasemonkey/greasemonkey

commit 59f07764b8c975cc3cef451743e3f2754fc95f45

Support .responseXML in GM_xmlhttpRequest().

modules/xmlhttprequester.js
@@ -102,9 +102,7 @@ function(safeUrl, details, req) {
   if (details.overrideMimeType) {
     req.overrideMimeType(details.overrideMimeType);
   }
-  if (details.responseType) {
-    req.responseType = details.responseType;
-  }
+
   if (details.timeout) {
     req.timeout = details.timeout;
   }
@@ -179,17 +177,17 @@ function(wrappedContentWin, req, event, details) {
           response: "r",
           responseHeaders: "r",
           responseText: "r",
+          responseXML: "r",
           status: "r",
           statusText: "r",
           total: "r",
           },
       context: details.context || null,
-      // Can't support responseXML because security won't
-      // let the browser call properties on it.
       readyState: req.readyState,
       response: req.response,
       responseHeaders: null,
       responseText: null,
+      responseXML: null,
       status: null,
       statusText: null,
       finalUrl: null
@@ -200,6 +198,13 @@ function(wrappedContentWin, req, event, details) {
     } catch (e) {
       // Some response types don't have .responseText (but do have e.g. blob
       // .response).  Ignore.
+    }
+
+    if (req.responseXML) {
+      // Adopt the XML object into a content-window-scoped document.
+      var xmlDoc = wrappedContentWin.Document();
+      xmlDoc.appendChild(xmlDoc.adoptNode(req.responseXML.documentElement));
+      responseState.responseXML = xmlDoc;
     }
 
     switch (event) {

commit 63db53b7022653ff1e3da2d0409da380a6816085

Restore the responseType feature for GM_xhr
Broken by 59f07764b8c975cc3cef451743e3f2754fc95f45 .

Fixes #2032

modules/xmlhttprequester.js
@@ -99,6 +99,9 @@ function(safeUrl, details, req) {
   if (details.overrideMimeType) {
     req.overrideMimeType(details.overrideMimeType);
   }
+  if (details.responseType) {
+    req.responseType = details.responseType;
+  }
 
   if (details.timeout) {
     req.timeout = details.timeout;
@@ -205,10 +208,16 @@ function(wrappedContentWin, req, event, details) {
       // .response).  Ignore.
     }
 
-    if (req.responseXML) {
+    var responseXML = null;
+    try {
+      responseXML = req.responseXML;
+    } catch (e) {
+      // Ignore failure.  At least in responseType blob case, this access fails.
+    }
+    if (responseXML) {
       // Clone the XML object into a content-window-scoped document.
       var xmlDoc = new wrappedContentWin.Document();
-      var clone = xmlDoc.importNode(req.responseXML.documentElement, true);
+      var clone = xmlDoc.importNode(responseXML.documentElement, true);
       xmlDoc.appendChild(clone);
       responseState.responseXML = xmlDoc;
     }