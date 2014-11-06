938591f81ef07670402b72e307cbed1fe6c13bbd
janekptacijarabaci <janekptacijarabaci@users.noreply.github.com>
2014-11-06 07:59:54 +0100

    GM_log truncates at NULL byte (typeof 'string' only)

--- modules/miscapis.js
+++ modules/miscapis.js
@@ -244,7 +244,8 @@ GM_ScriptLogger.prototype.consoleService = Components
 
 GM_ScriptLogger.prototype.log = function(message) {
   // https://developer.mozilla.org/en-US/docs/XPCOM_Interface_Reference/nsIConsoleService#logStringMessage() - wstring / wide string
-  message = message.replace(/\0/g, '');
+  if ('string' == typeof message || message instanceof String)
+    message = message.replace(/\0/g, '');
   this.consoleService.logStringMessage(this.prefix + '\n' + message);
 };
 
