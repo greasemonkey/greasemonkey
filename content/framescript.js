56749f4b741a29c72a0ba3aa640613369390ebab
janekptacijarabaci <janekptacijarabaci@users.noreply.github.com>
2015-04-19 12:38:03 +0200

    Update framescript.js

--- content/framescript.js
+++ content/framescript.js
@@ -76,6 +76,7 @@ ScriptRunner.prototype.registeredMenuCommand = function(aCommand) {
     frozen: aCommand.frozen,
     index: length - 1,
     name: aCommand.name,
+    scriptName: aCommand.scriptName,
     windowId: aCommand.contentWindowId
   });
 };
