baf5f138d125d889e1408186d2669ab442e92329
janekptacijarabaci <janekptacijarabaci@users.noreply.github.com>
2015-08-09 15:38:24 +0200

    Update browser.js

--- content/browser.js
+++ content/browser.js
@@ -79,12 +79,12 @@ GM_BrowserUI.openInTab = function(aMessage) {
 
   var getBool = Services.prefs.getBoolPref;
 
-  var prefBg = (aMessage.data.inBackground == null)
+  var prefBg = (aMessage.data.inBackground === null)
                ? getBool('browser.tabs.loadInBackground')
                : aMessage.data.inBackground;
   if (scriptTabIsCurrentTab && !prefBg) tabBrowser.selectedTab = newTab;
 
-  var prefRel = (aMessage.data.afterCurrent == null)
+  var prefRel = (aMessage.data.afterCurrent === null)
                 ? getBool('browser.tabs.insertRelatedAfterCurrent')
                 : aMessage.data.afterCurrent;
   if (prefRel) tabBrowser.moveTabTo(newTab, scriptTab._tPos + 1);
