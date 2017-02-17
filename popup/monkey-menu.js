06cb3d08f53344bbdc759e9207ff09152c755163
Anthony Lieuallen <arantius@gmail.com>
2017-02-17 14:27:38 -0500

    Remove console.log() calls.

--- popup/monkey-menu.js
+++ popup/monkey-menu.js
@@ -1,14 +1,11 @@
 function setMockIcons() {
   let icons = document.querySelectorAll('.panel .user-script .icon');
-  console.log('icons?', icons);
   for (let icon of icons) {
     while (icon.firstChild) icon.removeChild(icon.firstChild);
-    console.log(icon);
     let img = document.createElement('img');
     // If this were real, the name *and* icon would come from script data.
     img.src = browser.extension.getURL('skin/userscript.png');
     icon.appendChild(img);
-    console.log(icon, img);
   }
 }
 
