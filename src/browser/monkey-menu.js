8f6e0ace0b1375205310f1ed333b2b818dfc59a8
Anthony Lieuallen <arantius@gmail.com>
2017-04-07 10:39:20 -0400

    Code style: Less indentation.

--- src/browser/monkey-menu.js
+++ src/browser/monkey-menu.js
@@ -2,35 +2,32 @@
 
 const defaultIcon = browser.extension.getURL('skin/userscript.png');
 
-console.log('send');
-browser.runtime.sendMessage({'name': 'ListUserScripts'})
-    .then(userScripts => {
-      console.log('recv');
-      let containerEl = document.querySelector('#user-scripts');
-      for (let oldEl of containerEl.querySelectorAll('.user-script')) {
-        oldEl.parentNode.removeChild(oldEl);
-      }
+browser.runtime.sendMessage({'name': 'ListUserScripts'}).then(userScripts => {
+  let containerEl = document.querySelector('#user-scripts');
+  for (let oldEl of containerEl.querySelectorAll('.user-script')) {
+    oldEl.parentNode.removeChild(oldEl);
+  }
 
-      userScripts.sort((a, b) => a.name.localeCompare(b.name));
+  userScripts.sort((a, b) => a.name.localeCompare(b.name));
 
-      let empty = true;
-      let tplEl = document.querySelector('#templates .user-script > div');
-      for (let userScript of userScripts) {
-        empty = false;
-        let menuEl = tplEl.cloneNode(true);
+  let empty = true;
+  let tplEl = document.querySelector('#templates .user-script > div');
+  for (let userScript of userScripts) {
+    empty = false;
+    let menuEl = tplEl.cloneNode(true);
 
-        if (!userScript.enabled) menuEl.classList.append('disabled');
+    if (!userScript.enabled) menuEl.classList.append('disabled');
 
-        let icon = document.createElement('img');
-        icon.src = userScript.icon ? userScript.icon : defaultIcon;
-        menuEl.querySelector('.icon').appendChild(icon);
+    let icon = document.createElement('img');
+    icon.src = userScript.icon ? userScript.icon : defaultIcon;
+    menuEl.querySelector('.icon').appendChild(icon);
 
-        menuEl.querySelector('.name').textContent = userScript.name;
-        containerEl.appendChild(menuEl);
-      }
+    menuEl.querySelector('.name').textContent = userScript.name;
+    containerEl.appendChild(menuEl);
+  }
 
-      containerEl.style.display = empty ? 'none' : '';
-    });
+  containerEl.style.display = empty ? 'none' : '';
+});
 
 
 window.addEventListener('click', function(event) {
