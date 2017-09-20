6251c83fb7c887824d7b31c983646aeb25f4fb7c
Anthony Lieuallen <arantius@gmail.com>
2017-09-20 16:58:35 -0400

    Add welcome page tab, at first launch.

---
+++ src/bg/welcome.js
@@ -0,0 +1,19 @@
+(function() {
+const wantToShowUrl
+    = 'https://www.greasespot.net/2017/09/greasemonkey-4-announcement.html';
+
+chrome.storage.local.get('previousWelcomeUrl', items => {
+  let previousWelcomeUrl = items.previousWelcomeUrl;
+  if (previousWelcomeUrl != wantToShowUrl) {
+    chrome.storage.local.set({'previousWelcomeUrl': wantToShowUrl,}, () => {
+      if (chrome.runtime.lastError) {
+        console.warn(
+          'Could not set previousWelcomeUrl.', chrome.runtime.lastError);
+        return;
+      }
+      chrome.tabs.create({'url': wantToShowUrl});
+    });
+  }
+});
+
+})();
