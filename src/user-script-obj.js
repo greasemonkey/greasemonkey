e9829ebafbbdf4d48192c51324b375b7e2133c67
Anthony Lieuallen <arantius@gmail.com>
2017-09-28 09:35:02 -0400

    Do not mark the user script wrapper `async`.
    See https://github.com/greasemonkey/greasemonkey/issues/2275#issuecomment-332831929
    And https://github.com/tc39/ecmascript-asyncawait/issues/9
    
    This is a bad idea for compatibility.
    
    Refs #2275

--- src/user-script-obj.js
+++ src/user-script-obj.js
@@ -234,7 +234,7 @@ window.EditableUserScript = class EditableUserScript
     this._evalContent
         = 'try {'
         + '(function scopeWrapper(){'
-        + 'async function userScript(){' + this._content + '} // User Script End.\n\n'
+        + 'function userScript(){' + this._content + '} // User Script End.\n\n'
         + this.calculateGmInfo() + '\n\n'
         + apiProviderSource(this) + '\n\n'
         + Object.values(this._requiresContent).join('\n\n')
