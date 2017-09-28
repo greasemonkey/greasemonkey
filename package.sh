f68ccc0e579127e659dd95477f14654434cf1984
Anthony Lieuallen <arantius@gmail.com>
2017-09-28 12:42:25 -0400

    BSD compatible package.sh

--- package.sh
+++ package.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 set -e
-set -x
 
-VER=$(sed -n -e '/"version"/{ s/.*: "//; s/".*//; p; q}' manifest.json)
+VER=$(sed -n -e '/"version"/{ s/.*: "//; s/".*//; p; q; }' manifest.json)
+XPI="greasemonkey-${VER}.xpi"
 
-zip -r -9 greasemonkey-${VER}.xpi \
+echo "Packaging ${XPI} ..."
+rm -f "${XPI}"
+zip -q -r -9 "${XPI}" \
   _locales skin src LICENSE.mit manifest.json
