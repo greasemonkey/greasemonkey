greasemonkey/greasemonkey

commit 0459b05167f656ede8a542404cb64c1e4156dc7b

Revert "Alias unsafeWindow to window, for grant none scripts."
This reverts commit 7d78482325c586deb3a50a83e8b57172e508f51b.

Since we can't ask for no xrays (like we could back in 3.x), this seems to cause more problems than it helps.

src/user-script-obj.js
@@ -9,7 +9,7 @@ reference any other objects from this file.
 // Increment this number when updating `calculateEvalContent()`.  If it
 // is higher than it was when eval content was last calculated, it will
 // be re-calculated.
-const EVAL_CONTENT_VERSION = 3;
+const EVAL_CONTENT_VERSION = 4;
 
 
 // Private implementation.
@@ -241,16 +241,14 @@ window.EditableUserScript = class EditableUserScript
     // Put the first line of the script content on line one of the
     // generated content -- wrapped in a function.  Then add the rest
     // of the generated parts.
-    let hasGrantNone = this.grants.includes('none');
     this._evalContent
         // Note intentional lack of line breaks before the script content.
-        = `try { (function scopeWrapper(){ function userScript(window) { ${this._content} }
-
+        = `try { (function scopeWrapper(){ function userScript() { ${this._content} }
         const unsafeWindow = window.wrappedJSObject;
         ${this.calculateGmInfo()}
         ${apiProviderSource(this)}
         ${Object.values(this._requiresContent).join('\n\n')}
-        userScript(${ hasGrantNone ? 'unsafeWindow' : 'window' }); })();
+        userScript();
         } catch (e) { console.error("Script error: ", e); }
         //# sourceURL=user-script:${escape(this.id)}`;
     this._evalContentVersion = EVAL_CONTENT_VERSION;

commit 6b3cf5861f940fc291e25383319acc54e4ad0c28

Fixed incorrect paren wrapping for evalContent
Caused by 0459b05

src/user-script-obj.js
@@ -248,7 +248,7 @@ window.EditableUserScript = class EditableUserScript
         ${this.calculateGmInfo()}
         ${apiProviderSource(this)}
         ${Object.values(this._requiresContent).join('\n\n')}
-        userScript();
+        userScript(); })();
         } catch (e) { console.error("Script error: ", e); }
         //# sourceURL=user-script:${escape(this.id)}`;
     this._evalContentVersion = EVAL_CONTENT_VERSION;