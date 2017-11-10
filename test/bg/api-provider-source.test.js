133bef077be3c210479b73739c362e1e417d0c06
Anthony Lieuallen <arantius@gmail.com>
2017-11-10 16:16:19 -0500

    Basic api-provider-source test.

---
+++ test/bg/api-provider-source.test.js
@@ -0,0 +1,18 @@
+describe('api-provider-source', () => {
+  for (let apiName of SUPPORTED_APIS) {
+    it('handles ' + apiName, () => {
+      let source = apiProviderSource({'grants': [apiName]});
+      assert(source.match(new RegExp(apiName + ' = ')));
+    });
+  }
+
+  it('handles grant none', () => {
+    let source = apiProviderSource({'grants': ['none']});
+    assert(source.match(/No grants/));
+  });
+
+  it('handles no grants', () => {
+    let source = apiProviderSource({'grants': []});
+    assert(source.match(/No grants/));
+  });
+});
