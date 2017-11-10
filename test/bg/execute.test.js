0e5e4c4634d05a25ab356e5e6c689ba17cbde612
Anthony Lieuallen <arantius@gmail.com>
2017-11-10 16:26:22 -0500

    Basic bg/execute test.

---
+++ test/bg/execute.test.js
@@ -0,0 +1,11 @@
+describe('execute', () => {
+  before(() => sinon.stub(UserScriptRegistry, 'scriptsToRunAt'));
+  after(() => UserScriptRegistry.scriptsToRunAt.restore());
+
+  it('uses tabs.executeScript', () => {
+    chrome.tabs.executeScript.callsArg(2);
+    UserScriptRegistry.scriptsToRunAt.returns([{}]);
+    executeUserscriptOnNavigation({});
+    assert(chrome.tabs.executeScript.calledOnce);
+  });
+});
