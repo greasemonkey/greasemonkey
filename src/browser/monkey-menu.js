f827e199be0db6c895438ac66fb055b132c30afd
Steven Daniele <sdaniele3@gatech.edu>
2017-11-11 14:30:38 -0600

    Replace monkey menu divs with a tags
    Partial implementation for #2639.
    Basic keyboard navigation is supported. Up/down arrows.

--- src/browser/monkey-menu.js
+++ src/browser/monkey-menu.js
@@ -19,6 +19,20 @@ const gNewScriptTpl = `// ==UserScript==
 // @grant    none
 // ==/UserScript==`;
 
+// Keep global state for keyboard navigation
+let gTopMenuSelection = 0;
+let gTopMenuTags;
+let gScriptMenuSelection = 0;
+let gScriptMenuTags;
+// Prevent variables from entering scope
+(function() {
+  let userScriptTopSection = document.getElementById('menu');
+  gTopMenuTags = userScriptTopSection.getElementsByTagName('A');
+
+  let userScriptDetailSection = document.getElementById('user-script-detail');
+  gScriptMenuTags = userScriptDetailSection.getElementsByTagName('A');
+})()
+
 ///////////////////////////////////////////////////////////////////////////////
 
 //I.e. from a script detail view, go back to the top view.
@@ -27,6 +41,8 @@ function goToTop() {
   checkPendingUninstall();
   document.body.className = '';
   gActiveUuid = null;
+  gScriptMenuSelection = 0;
+  focusSelection();
 }
 
 
@@ -49,54 +65,53 @@ function loadScripts(userScriptsDetail, url) {
 }
 
 
-function onClick(event) {
-  let el = event.target;
+// Catch when a link has been 'navigated'
+function onHashChange(event) {
+  event.preventDefault();
 
-  if (el.id == 'back') {
-    goToTop();
-    return;
-  }
+  let hash = location.hash;
+  // Encode the command behind the first underscore (_)
+  let command = hash.split('_', 1)[0];
+  // Encode any parameters as the rest of the string
+  let parameter = hash.slice(command.length + 1);
 
-  while (el && el.classList && !el.classList.contains('menu-item')) {
-    el = el.parentNode;
-  }
-  if (!el || !el.classList || !el.classList.contains('menu-item')) {
-    console.warn('monkey menu got click on non-menu item:', event.target);
-    return;
-  }
-
-  if (el.hasAttribute('data-url')) {
-    chrome.tabs.create({
-      'active': true,
-      'url': el.getAttribute('data-url')
-    });
-    window.close();
-  } else if (el.hasAttribute('data-user-script-uuid')) {
-    let uuid = el.getAttribute('data-user-script-uuid');
-    let userScript = gUserScripts[uuid];
-
-    gTplData.activeScript.icon = iconUrl(userScript);
-    gTplData.activeScript.enabled = userScript.enabled;
-    gTplData.activeScript.name = userScript.name;
-
-    gActiveUuid = uuid;
-    document.body.className = 'detail';
-  } else switch (el.getAttribute('id')) {
-    case 'toggle-global-enabled':
-      chrome.runtime.sendMessage(
-          {'name': 'EnabledToggle'},
-          enabled => gTplData.enabled = enabled);
+  switch (command) {
+    case '#open-tab':
+      chrome.tabs.create({
+        'active': true,
+        'url': parameter,
+      });
+      window.close();
       break;
+    case '#menu-top':
+      goToTop();
+      break;
+    case '#menu-uuid':
+      let uuid = parameter;
+      let userScript = gUserScripts[uuid];
 
-    case 'new-user-script':
+      gTplData.activeScript.icon = iconUrl(userScript);
+      gTplData.activeScript.enabled = userScript.enabled;
+      gTplData.activeScript.name = userScript.name;
+
+      gActiveUuid = uuid;
+      document.body.className = 'detail';
+      break;
+    case '#toggle-global':
+      chrome.runtime.sendMessage(
+        {'name': 'EnabledToggle'},
+        enabled => gTplData.enabled = enabled);
+      // Replace the history state
+      history.replaceState({}, 'Home', '#menu-top');
+      break;
+    case '#new-user-script':
       let r = Math.floor(Math.random() * 900000 + 100000);
       let newScriptSrc = gNewScriptTpl.replace('%d', r);
       chrome.runtime.sendMessage(
           {'name': 'UserScriptInstall', 'source': newScriptSrc},
           uuid => openUserScriptEditor(uuid));
       break;
-
-    case 'user-script-toggle-enabled':
+    case '#toggle-user-script':
       chrome.runtime.sendMessage({
         'name': 'UserScriptToggleEnabled',
         'uuid': gActiveUuid,
@@ -105,24 +120,48 @@ function onClick(event) {
         gTplData.activeScript.enabled = response.enabled;
         tplItemForUuid(gActiveUuid).enabled = response.enabled;
       });
+      // Replace the history state
+      history.replaceState({}, 'Home', '#menu-uuid_' + gActiveUuid);
       break;
-    case 'user-script-edit':
+    case '#edit-user-script':
       openUserScriptEditor(gActiveUuid);
       window.close();
       break;
-
-    case 'user-script-uninstall':
+    case '#uninstall-user-script':
       gTplData.pendingUninstall = 10;
+      focusSelection();
       break;
-    case 'user-script-uninstall-undo':
+    case '#undo-uninstall-user-script':
       gTplData.pendingUninstall = null;
+      focusSelection();
       break;
-
     default:
-      console.warn('unhandled monkey menu item:', el);
+      console.warn('Unhandled Monkey Menu href:', hash);
       break;
   }
 }
+
+
+function onKeypress(event) {
+  let key = event.key;
+  if ('Enter' === key) {
+    return;
+  }
+  event.preventDefault();
+
+  let increment;
+  switch (key) {
+    case 'ArrowUp':
+      increment = -1;
+    case 'ArrowDown':
+      increment = increment || 1;
+
+      incrementIndex(increment);
+      focusSelection();
+      break;
+    }
+}
+
 
 function onLoad(event) {
   gPendingTicker = setInterval(pendingUninstallTicker, 1000);
@@ -169,6 +208,68 @@ function tplItemForUuid(uuid) {
   }
 }
 
+////////////////////////////////// KEYBOARD \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+
+// Normalize the index to fit between 0 and the provided value
+function normalizeIndex(index, max) {
+  if (index >= max) {
+    return max - 1;
+  } else if (index < 0) {
+    return 0;
+  } else {
+    return index;
+  }
+}
+
+
+// If a Uuid is active then change the script selection otherwise change the
+// top menu selection.
+function incrementIndex(amount) {
+  if (gActiveUuid) {
+    let which = gScriptMenuSelection + amount;
+    gScriptMenuSelection = normalizeIndex(which, gScriptMenuTags.length - 1);
+  } else {
+    let index = gTopMenuSelection + amount;
+    gTopMenuSelection = normalizeIndex(index, gTopMenuTags.length);
+  }
+}
+
+
+// Focus a menu item based on if Uuid is active and the current selection
+// value.
+function focusSelection() {
+  if (gActiveUuid) {
+    // For highlighting the uninstall / undo uninstall
+    if ('none' == gScriptMenuTags[gScriptMenuSelection].style.display) {
+      gScriptMenuTags[gScriptMenuSelection+1].focus();
+    } else {
+      gScriptMenuTags[gScriptMenuSelection].focus();
+    }
+  } else {
+    gTopMenuTags[gTopMenuSelection].focus();
+  }
+}
+
+function onKeypress(event) {
+  let key = event.key;
+  if ('Enter' === key) {
+    return;
+  }
+  event.preventDefault();
+
+  let increment;
+  switch (key) {
+    case 'ArrowUp':
+      increment = -1;
+    case 'ArrowDown':
+      increment = increment || 1;
+
+      incrementIndex(increment);
+      focusSelection();
+      break;
+    }
+}
+
 ////////////////////////////////// UNINSTALL \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 
 function checkPendingUninstall() {
@@ -197,6 +298,15 @@ function uninstall(scriptUuid) {
       let script = gTplData.userScripts[i];
       if (script.uuid == scriptUuid) {
         gTplData.userScripts.splice(i, 1);
+        break;
+      }
+    }
+    // Remove the element from the list of top menu tags
+    for (i in gTopMenuTags) {
+      let uuid = gTopMenuTags.getAttribute('data-uuid');
+      if (uuid == scriptUuid) {
+        gTopMenuTags.splice(i, 1);
+        gTopMenuSelection = normalizeIndex(i - 1, gTopMenuTags.length);
         break;
       }
     }
--- src/util/rivets-formatters.js
+++ src/util/rivets-formatters.js
@@ -2,3 +2,4 @@ rivets.formatters.empty = value => value.length == 0;
 rivets.formatters.active = value => value ? 'active' : 'disabled';
 rivets.formatters.enabled = value => value ? 'Enabled' : 'Disabled';
 rivets.formatters.not = value => !value;
+rivets.formatters.mmUuidMenu = value => '#menu-uuid_' + value;
