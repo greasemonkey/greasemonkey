fd7725036d0aa1b4f3baa04e1999ea39836a2d45
badbrainz <wormboy.d@gmail.com>
2017-11-19 21:56:04 -0800

    Move formatting to the helper function
    doc.findWordAt returns {anchor,head} of '.' in '@.foo' which isn't
    expected, so grab the word from metaLine instead.

--- src/content/cm-addons/linter.js
+++ src/content/cm-addons/linter.js
@@ -19,12 +19,22 @@ CodeMirror.registerHelper('lint', 'javascript', function (text, options) {
 
       if (s) window.parseMetaLine(s);
     } catch (e) {
-      messages.push({
+      let ann = {
         severity: 'warning',
         from: CodeMirror.Pos(metaStart + i, e.location.start.column - 1),
         to: CodeMirror.Pos(metaStart + i, e.location.end.column - 1),
         message: e.message
-      });
+      };
+      // Make "Expected...but 'f' found" more meaningful. If all characters
+      // after '// @' are whitespace e.found will be null so skip this step,
+      // else grab the word after '@' and insert it into the message.
+      if (ann.from.ch == 4 && e.found) {
+        let s = metaLine.slice(4).match(/[^\s]*/)[0];
+        ann.message = ann.message.replace(/(.+")([^"]+)(".+)$/, `$1${s}$3`);
+        // Force text marker to stretch the word so it's easier to hover over.
+        ann.to.ch = ann.from.ch + s.length;
+      }
+      messages.push(ann);
     }
   }
   return messages;
