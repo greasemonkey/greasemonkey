4a8351436ba8abf97c6bb283adf68f78acebd835
NoneGiven <NoneGiven@users.noreply.github.com>
2017-12-01 09:43:39 -0500

    Fix copy-paste error

--- test/third-party/MatchPattern.test.js
+++ test/third-party/MatchPattern.test.js
@@ -1,14 +1,4 @@
-describe('bg/user-script-registry', () => {
-  beforeEach(() => {
-    indexedDB.deleteDatabase('greasemonkey');
-  });
-
-  function scriptNamed(name) {
-    for (let userScript of UserScriptRegistry.scriptsToRunAt()) {
-      if (userScript.name === name) return userScript;
-    }
-  }
-
+describe('third-party/MatchPattern', () => {
   it('doMatch respects URL query string', async () => {
     let withQuery = new MatchPattern("https://example.com/test?query=*");
     assert.isOk(withQuery.doMatch(
