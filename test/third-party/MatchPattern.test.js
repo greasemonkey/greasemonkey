8c15a0b7b88f7cb8036f57a7009dc6968d8e5dbd
Anthony Lieuallen <arantius@gmail.com>
2017-12-04 15:25:52 -0500

    Correctly match URLs-with-ports in MatchPattern.
    Fixes #2673

--- test/third-party/MatchPattern.test.js
+++ test/third-party/MatchPattern.test.js
@@ -1,14 +1,35 @@
 describe('third-party/MatchPattern', () => {
-  it('doMatch respects URL query string', async () => {
-    let withQuery = new MatchPattern("https://example.com/test?query=*");
-    assert.isOk(withQuery.doMatch(
-      new URL("https://example.com/test?query=thing")));
-    assert.isNotOk(withQuery.doMatch(
-      new URL("https://example.com/test")));
-    let noQuery = new MatchPattern("https://example.com/test");
-    assert.isOk(noQuery.doMatch(
-      new URL("https://example.com/test")));
-    assert.isNotOk(noQuery.doMatch(
-      new URL("https://example.com/test?query=thing")));
+  const matches
+      = (pattern, urlStr) => assert.isOk(pattern.doMatch(new URL(urlStr)));
+  const notMatches
+      = (pattern, urlStr) => assert.isNotOk(pattern.doMatch(new URL(urlStr)));
+
+  describe('doMatch()', () => {
+    it('works when query string is present', async () => {
+      let pattern = new MatchPattern('https://example.com/test?query=*');
+      notMatches(pattern, 'https://example.com/test');
+      matches(pattern, 'https://example.com/test?query=thing');
+    });
+
+    it('works when query string is not present', async () => {
+      let pattern = new MatchPattern('https://example.com/test');
+      matches(pattern, 'https://example.com/test');
+      notMatches(pattern, 'https://example.com/test?query=thing');
+    });
+
+    it('works with ports in the URL', () => {
+      let pattern = new MatchPattern('http://example.net/*');
+      matches(pattern, 'http://example.net/');
+      notMatches(pattern, 'http://otherexample.net/');
+      matches(pattern, 'http://example.net:1234/');
+    });
+
+    it('works with subdomains and ports in the URL', () => {
+      let pattern = new MatchPattern('http://*.example.net/*');
+      matches(pattern, 'http://sub.example.net/');
+      matches(pattern, 'http://sub.sub.example.net/');
+      notMatches(pattern, 'http://sub.otherexample.net/');
+      matches(pattern, 'http://sub.example.net:1234/');
+    });
   });
 });
