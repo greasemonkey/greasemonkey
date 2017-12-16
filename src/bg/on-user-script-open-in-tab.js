b01871cf085430db20363b3f75813c2cd0d25046
100の人 <100@pokemori.jp>
2017-12-16 17:38:06 +0900

    Avoid the error which be occured by `opennerTabId` in Firefox 52 ESR.
    Additionally, removes unnecessary `tabs.Tab.windowId` and `tabs.Tab.index` if `tabs.Tab.opennerTabId` exists.
    
    These complete https://github.com/greasemonkey/greasemonkey/commit/256bf162037639e101bd432f3d7adff5d00a79dd and resolves https://github.com/greasemonkey/greasemonkey/issues/2672 as a result.

--- src/bg/on-user-script-open-in-tab.js
+++ src/bg/on-user-script-open-in-tab.js
@@ -5,11 +5,18 @@ This file is responsible for providing the GM.openInTab API method.
 function onApiOpenInTab(message, sender, sendResponse) {
   checkApiCallAllowed('GM.openInTab', message.uuid);
   const senderTab = sender.tab;
-  chrome.tabs.create({
+  const tab = {
     url: message.url,
     active: message.active,
-    windowId: senderTab.windowId,
-    index: senderTab.index + 1, // next to senderTab
-    openerTabId: senderTab.id,
+  };
+  chrome.runtime.getBrowserInfo(browserInfo => {
+    if (browserInfo.name === 'Firefox' && browserInfo.version.split('.')[0] < 57) {
+      tab.windowId = senderTab.windowId;
+      tab.index = senderTab.index + 1; // next to senderTab
+    } else {
+      tab.openerTabId = senderTab.id;
+    }
+
+    chrome.tabs.create(tab);
   });
 };
