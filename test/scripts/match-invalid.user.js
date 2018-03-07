8170ada7c5b2b6fb33ec00e8db8bf455daa111c5
Anthony Lieuallen <arantius@gmail.com>
2018-03-07 10:55:12 -0500

    Test script with invalid `@match`.

---
+++ test/scripts/match-invalid.user.js
@@ -0,0 +1,6 @@
+// ==UserScript==
+// @name        Invalid @match test
+// @description This user script includes a `@match` with invalid syntax.
+// @grant       none
+// @match       http*://localhost/*
+// ==/UserScript==
