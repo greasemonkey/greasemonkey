33ffd10c2b4c78e4992ebb5e1a23a419b1d2f808
Anthony Lieuallen <arantius@gmail.com>
2018-03-13 11:10:50 -0400

    Add "missing icon" test script.

---
+++ test/scripts/missing-icon.user.js
@@ -0,0 +1,7 @@
+// ==UserScript==
+// @name        Missing icon test.
+// @description Exercises the `@icon` directive, pointing at an invalid (will 404) URL.  Should show the default script icon.
+// @grant       none
+// @icon        404.png
+// ==/UserScript==
+
