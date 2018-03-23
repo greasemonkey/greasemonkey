337e893bba4f2b60f63873ba86bd723f580f7f0e
Anthony Lieuallen <arantius@gmail.com>
2018-03-23 14:15:24 -0400

    Two test scripts for `@version`.
    Including one with a "long version" (based on date), which currently does not display properly.

---
+++ test/scripts/version-long.user.js
@@ -0,0 +1,6 @@
+// ==UserScript==
+// @name        Version long test
+// @description Exercises the `@version` directive.  Should show in the install dialog at minimum.  Has a long value.
+// @grant       none
+// @version     2018.03.23.1414
+// ==/UserScript==
---
+++ test/scripts/version.user.js
@@ -0,0 +1,6 @@
+// ==UserScript==
+// @name        Version test
+// @description Exercises the `@version` directive.  Should show in the install dialog at minimum.
+// @grant       none
+// @version     1.0
+// ==/UserScript==
