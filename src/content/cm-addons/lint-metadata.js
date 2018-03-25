c9992a3f2e43ff8ac4fd88244bf5a38aec4768b7
Eselce <S.Loges@gmx.de>
2018-03-25 22:11:59 +0200

    Resolve review remarks

--- src/content/cm-addons/lint-metadata.js
+++ src/content/cm-addons/lint-metadata.js
@@ -16,7 +16,7 @@ function lintMetadata(text, options) {
   for (let [i, metaLine] of meta.entries()) {
     try {
       // Only consider meta lines.
-      if (metaLine.substr(0, META_POSN) === META_PREFIX) {
+      if (metaLine.startsWith(META_PREFIX)) {
         parseMetaLine(metaLine.trim());
       }
     } catch (e) {
@@ -24,12 +24,14 @@ function lintMetadata(text, options) {
         'severity': 'warning',
         'from': { line: metaStartLine + i, ch: e.location.start.column - 1 },
         'to': { line: metaStartLine + i, ch: e.location.end.column - 1 },
-        'message': e.message
+        'message': e.message,
       };
       // Turn "Expected...but 'w' found" into "Expected...but 'word' found".
-      if (e.message.match(/Expected .* but .* found\./) && ann.from.ch === META_POSN) {
+      if (e.message.match(/Expected .* but .* found\./) &&
+          ann.from.ch === META_POSN) {
         let m = metaLine.substr(META_POSN).match(/^(\s+|\S*)/);
-        ann.message = ann.message.replace(/( but ")([^"]*)(" found\.)$/, `$1${m[1]}$3`);
+        ann.message = ann.message
+            .replace(/( but ")([^"]*)(" found\.)$/, `$1${m[1]}$3`);
         ann.to.ch = ann.from.ch + m[1].length;
       }
       messages.push(ann);
