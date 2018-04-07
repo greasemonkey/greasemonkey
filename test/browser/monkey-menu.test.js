6efffbe9f99f135557454f86bf9b1ab0c7de97b9
Steven Daniele <sdaniele3@gatech.edu>
2018-04-07 12:10:25 -0400

    Remove 'syntax' unit tests
    Syntax should be checked using a linter not by arbitrarily loading
    files.

--- test/browser/monkey-menu.test.js
+++ test/browser/monkey-menu.test.js
@@ -1,22 +1,5 @@
 'use strict';
 describe('browser/monkey-menu', () => {
-  it('has no syntax errors in activate()', () => {
-    activate(document.createElement('div'));
-  });
-
-  it('has no syntax errors in loadScripts() and '
-  + 'toggleUserScriptEnabled() and uninstall()', () => {
-    loadScripts(
-        [{'name': 'binary', 'uuid': 'fake-uuid'}],
-        new URL('http://example.com/'));
-
-    chrome.runtime.sendMessage.callsArgWith(1, {'enabled': true});
-    toggleUserScriptEnabled('fake-uuid')
-
-    chrome.runtime.sendMessage.callsArg(1);
-    uninstall('fake-uuid')
-  });
-
   it('creates a tab for openUserScriptEditor()', () => {
     chrome.tabs.create.reset();
     openUserScriptEditor('fake-uuid');
