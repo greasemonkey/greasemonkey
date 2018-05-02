585edce67bca289b141bd6efdda5d9cb3cdce97a
Anthony Lieuallen <arantius@gmail.com>
2018-05-02 15:49:24 -0400

    Test script: long description.
    Refs #2927

---
+++ test/scripts/long-description.user.js
@@ -0,0 +1,8 @@
+'use strict';
+// ==UserScript==
+// @name        Test User Script With Long Description
+// @description This description is so long that can be used to guarantee that any UI displaying it is capable of coping with extremely long description values.  Imperdiet purus natoque porta hendrerit, sociosqu lacinia congue nulla eu libero vitae ridiculus. Metus primis, inceptos aliquet dapibus tellus suspendisse. At diam enim cras justo eget viverra, posuere libero sed gravida vestibulum. Faucibus venenatis sociosqu. Litora pulvinar parturient ultrices dictum. Volutpat lobortis fringilla tempus. Magnis magna.  Turpis hendrerit justo tellus id accumsan vulputate. Purus nulla, est habitant massa. Consectetuer torquent magnis senectus integer lacus pulvinar sociosqu ridiculus urna. Sociosqu adipiscing. Placerat nulla iaculis tristique sapien cum pulvinar. Dapibus enim hymenaeos wisi primis potenti curae. Ridiculus elementum ornare pretium. Velit volutpat. Et euismod. Tempus tincidunt nulla torquent egestas eleifend ultrices. Iaculis lacinia, senectus nunc wisi purus viverra magna ligula purus. Tellus conubia odio elit. Lectus tincidunt. Ante, primis per platea non aliquet eget tincidunt rutrum aliquam.
+// @version     1.0
+// @grant       none
+// ==/UserScript==
+
