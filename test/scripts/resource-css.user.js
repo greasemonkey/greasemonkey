adfb4b9af36999d3c0063d9df6859dadebcef993
Anthony Lieuallen <arantius@gmail.com>
2018-05-18 11:21:54 -0400

    Fix relative `@resource` URL.

--- test/scripts/resource-css.user.js
+++ test/scripts/resource-css.user.js
@@ -3,6 +3,5 @@
 // @name        Resource CSS test.
 // @description Exercises the `@resource` directive, but does nothing with it.
 // @grant       none
-// @resource    CSS dep/resource.css
+// @resource    CSS resource.css
 // ==/UserScript==
-
