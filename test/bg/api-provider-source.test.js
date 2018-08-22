834b344f1d72c064a40dabd17a98fcb03316ee12
Chris <lwchris@gmx.de>
2018-08-22 06:18:59 +0200

    Properly escape regex strings

--- test/bg/api-provider-source.test.js
+++ test/bg/api-provider-source.test.js
@@ -3,11 +3,11 @@ describe('bg/api-provider-source', () => {
   for (let apiName of SUPPORTED_APIS) {
     it('handles ' + apiName, () => {
       let source = apiProviderSource({'grants': [apiName]});
-      assert(!source.match(new RegExp(apiName + ' = () => { _notGranted("')));
+      assert(!source.match(new RegExp(apiName + ' = \\(\\) => \\{ _notGranted\\("')));
       assert(source.match(new RegExp(apiName + ' = function GM_')));
       for (let otherApiName of SUPPORTED_APIS) {
         if (otherApiName == apiName) { continue; }
-        assert(source.match(new RegExp(otherApiName + ' = () => { _notGranted("')));
+        assert(source.match(new RegExp(otherApiName + ' = \\(\\) => \\{ _notGranted\\("')));
         assert(!source.match(new RegExp(otherApiName + ' = function GM_')));
       }
     });
@@ -16,7 +16,7 @@ describe('bg/api-provider-source', () => {
   it('handles grant none', () => {
     let source = apiProviderSource({'grants': ['none']});
     for (let apiName of SUPPORTED_APIS) {
-      assert(source.match(new RegExp(apiName + ' = () => { _notGranted("')));
+      assert(source.match(new RegExp(apiName + ' = \\(\\) => \\{ _notGranted\\("')));
       assert(!source.match(new RegExp(apiName + ' = function GM_')));
     }
   });
@@ -24,7 +24,7 @@ describe('bg/api-provider-source', () => {
   it('handles no grants', () => {
     let source = apiProviderSource({'grants': []});
     for (let apiName of SUPPORTED_APIS) {
-      assert(source.match(new RegExp(apiName + ' = () => { _notGranted("')));
+      assert(source.match(new RegExp(apiName + ' = \\(\\) => \\{ _notGranted\\("')));
       assert(!source.match(new RegExp(apiName + ' = function GM_')));
     }
   });
