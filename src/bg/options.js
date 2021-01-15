943a0dc251c8c5798288bcb1d9a59f005b77dd2b
Soldy <4786022+Soldy@users.noreply.github.com>
2021-01-15 15:13:17 +0000

    ion monkey crash fix
    Signed-off-by: Soldy <4786022+Soldy@users.noreply.github.com>

--- src/bg/options.js
+++ src/bg/options.js
@@ -7,120 +7,119 @@ of Greasemonkey.
 // Private implementation.
 (function() {
 
-let gIsEnabled = true;
-chrome.storage.local.get('globalEnabled', v => {
-  gIsEnabled = v['globalEnabled'];
-  if ('undefined' == typeof gIsEnabled) gIsEnabled = true;
-  setIcon();
-});
+    let gIsEnabled = true;
+    chrome.storage.local.get('globalEnabled', v => {
+        if ('undefined' !== typeof v['globalEnabled'])
+            gIsEnabled = v['globalEnabled'];
+        setIcon();
+    });
 
-let gGlobalExcludes = [];
-chrome.storage.local.get('globalExcludes', v => {
-  let str = v['globalExcludes'];
-  if ('undefined' != typeof str) {
-    gGlobalExcludes = str.split('\n');
-  }
-});
+    let gGlobalExcludes = [];
+    chrome.storage.local.get('globalExcludes', v => {
+        let str = v['globalExcludes'];
+        if ('undefined' !== typeof str) 
+            gGlobalExcludes = str.split('\n');
+    });
 
-let gUseCodeMirror = true;
-chrome.storage.local.get('useCodeMirror', v => {
-  gUseCodeMirror = v['useCodeMirror'];
-  if ('undefined' == typeof gUseCodeMirror) gUseCodeMirror = true;
-});
+    let gUseCodeMirror = true;
+    chrome.storage.local.get('useCodeMirror', v => {
+        if ('undefined' !== typeof v['useCodeMirror'])
+            gUseCodeMirror = v['useCodeMirror'];
+    });
 
-function getGlobalEnabled() {
-  return !!gIsEnabled;
-}
-window.getGlobalEnabled = getGlobalEnabled;
-
-
-function getGlobalExcludes() {
-  return gGlobalExcludes.slice();
-}
-window.getGlobalExcludes = getGlobalExcludes;
-
-
-function onEnabledQuery(message, sender, sendResponse) {
-  sendResponse(gIsEnabled);
-}
-window.onEnabledQuery = onEnabledQuery;
-
-
-function setGlobalEnabled(enabled) {
-  gIsEnabled = !!enabled;
-  chrome.runtime.sendMessage({
-    'name': 'EnabledChanged',
-    'enabled': gIsEnabled,
-  }, logUnhandledError);
-  setIcon();
-  chrome.storage.local.set({'globalEnabled': enabled});
-}
-window.setGlobalEnabled = setGlobalEnabled;
-function onEnabledSet(message, sender, sendResponse) {
-  setGlobalEnabled(message.enabled);
-}
-window.onEnabledSet = onEnabledSet;
-
-
-function setIcon() {
-  // Firefox for Android does not have setIcon
-  // https://developer.mozilla.org/en-US/Add-ons/WebExtensions/API/browserAction/setIcon#Browser_compatibility
-  if (!chrome.browserAction.setIcon) {
-    return;
-  }
-  let iconPath = chrome.extension.getURL('skin/icon.svg');
-  if (gIsEnabled) {
-    chrome.browserAction.setIcon({'path': iconPath});
-  } else {
-    let img = document.createElement('img');
-    img.onload = function() {
-      let canvas = document.createElement('canvas');
-      let ctx = canvas.getContext('2d');
-      ctx.globalAlpha = 0.5;
-      ctx.drawImage(img, 0, 0);
-      chrome.browserAction.setIcon({
-        'imageData': ctx.getImageData(0, 0, img.width, img.height),
-      });
+    const getGlobalEnabled = function(){
+        return !!gIsEnabled;
     };
-    img.src = iconPath;
-  }
-}
+    window.getGlobalEnabled = getGlobalEnabled;
 
 
-function toggleGlobalEnabled() {
-  setGlobalEnabled(!gIsEnabled);
-}
-window.toggleGlobalEnabled = toggleGlobalEnabled;
-
-/*****************************************************************************/
-
-function onEnabledToggle(message, sender, sendResponse) {
-  toggleGlobalEnabled();
-  sendResponse(gIsEnabled);
-}
-window.onEnabledToggle = onEnabledToggle;
+    const getGlobalExcludes = function() {
+        return gGlobalExcludes.slice();
+    };
+    window.getGlobalExcludes = getGlobalExcludes;
 
 
-function onOptionsLoad(message, sender, sendResponse) {
-  let options = {
-    'excludes': gGlobalExcludes.join('\n'),
-    'useCodeMirror': gUseCodeMirror,
-  };
-  sendResponse(options);
-}
-window.onOptionsLoad = onOptionsLoad;
+    const onEnabledQuery = function(message, sender, sendResponse) {
+        sendResponse(gIsEnabled);
+    };
+    window.onEnabledQuery = onEnabledQuery;
 
 
-function onOptionsSave(message, sender, sendResponse) {
-  chrome.storage.local.set(
-      {
-        'globalExcludes': message.excludes,
-        'useCodeMirror': message.useCodeMirror,
-        },
-      logUnhandledError);
-  gGlobalExcludes = message.excludes.split('\n');
-  gUseCodeMirror = message.useCodeMirror;
-}
-window.onOptionsSave = onOptionsSave;
+    const setGlobalEnabled = function(enabled) {
+        gIsEnabled = !!enabled;
+        chrome.runtime.sendMessage({
+            'name': 'EnabledChanged',
+            'enabled': gIsEnabled,
+        }, logUnhandledError);
+        setIcon();
+        chrome.storage.local.set({'globalEnabled': enabled});
+    };
+    window.setGlobalEnabled = setGlobalEnabled;
+    const onEnabledSet = function (message, sender, sendResponse) {
+        setGlobalEnabled(message.enabled);
+    };
+    window.onEnabledSet = onEnabledSet;
+
+
+    const  setIcon = function() {
+        // Firefox for Android does not have setIcon
+        // https://developer.mozilla.org/en-US/Add-ons/WebExtensions/API/browserAction/setIcon#Browser_compatibility
+        if (!chrome.browserAction.setIcon) {
+            return;
+        }
+        let iconPath = chrome.extension.getURL('skin/icon.svg');
+        if (gIsEnabled) {
+            chrome.browserAction.setIcon({'path': iconPath});
+        } else {
+            let img = document.createElement('img');
+            img.onload = function() {
+                let canvas = document.createElement('canvas');
+                let ctx = canvas.getContext('2d');
+                ctx.globalAlpha = 0.5;
+                ctx.drawImage(img, 0, 0);
+                chrome.browserAction.setIcon({
+                    'imageData': ctx.getImageData(0, 0, img.width, img.height),
+                });
+            };
+            img.src = iconPath;
+        }
+    };
+
+
+    const toggleGlobalEnabled = function() {
+        setGlobalEnabled(!gIsEnabled);
+    };
+    window.toggleGlobalEnabled = toggleGlobalEnabled;
+
+    /*****************************************************************************/
+
+    const onEnabledToggle = function (message, sender, sendResponse) {
+        toggleGlobalEnabled();
+        sendResponse(gIsEnabled);
+    };
+    window.onEnabledToggle = onEnabledToggle;
+
+
+    const onOptionsLoad = function (message, sender, sendResponse) {
+        let options = {
+            'excludes': gGlobalExcludes.join('\n'),
+            'useCodeMirror': gUseCodeMirror,
+        };
+        sendResponse(options);
+    };
+    window.onOptionsLoad = onOptionsLoad;
+
+
+    const onOptionsSave = function(message, sender, sendResponse) {
+        chrome.storage.local.set(
+            {
+                'globalExcludes': message.excludes,
+                'useCodeMirror': message.useCodeMirror,
+            },
+            logUnhandledError);
+        gGlobalExcludes = message.excludes.split('\n');
+        gUseCodeMirror = message.useCodeMirror;
+    };
+    window.onOptionsSave = onOptionsSave;
 
 })();
