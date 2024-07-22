7444f226643e75c8dc964309b7dd03046238544c
Anthony Lieuallen <arantius@gmail.com>
2024-07-22 13:58:19 -0400

    Make `GM.openInTab()` compatible with Firefox for Android.
    Since FfA does not support `openerTabId`, set that only conditionally.
    
    Fixes #1389

--- src/bg/on-user-script-open-in-tab.js
+++ src/bg/on-user-script-open-in-tab.js
@@ -5,19 +5,16 @@ This file is responsible for providing the GM.openInTab API method.
 
 function onApiOpenInTab(message, sender, sendResponse) {
   checkApiCallAllowed('GM.openInTab', message.uuid);
-  const senderTab = sender.tab;
-  const tab = {
+  let tab = {
     url: message.url,
     active: message.active,
   };
-  chrome.runtime.getBrowserInfo(browserInfo => {
-    if (browserInfo.name === 'Firefox' && browserInfo.version.split('.')[0] < 57) {
-      tab.windowId = senderTab.windowId;
-      tab.index = senderTab.index + 1; // next to senderTab
-    } else {
-      tab.openerTabId = senderTab.id;
+  chrome.runtime.getPlatformInfo(platform => {
+    if ('android' != platform.os) {
+      // https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/API/tabs/Tab#browser_compatibility
+      // Firefox for Android does not support `openerTabId`.
+      tab.openerTabId = sender.tab.id;
     }
-
     chrome.tabs.create(tab);
   });
   sendResponse();
